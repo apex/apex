6ca99397e1f57582f13ae303d7ebd1e3658a6c90
Maciej Winnicki <maciej.winnicki@gmail.com>
2016-01-04 10:58:11 +0100

    Delete unit tests

---
+++ function/function_test.go
@@ -0,0 +1,47 @@
+package function_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/apex/apex/function"
+	"github.com/apex/apex/mock"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDelete_Success(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	serviceMock := mock_lambdaiface.NewMockLambdaAPI(mockCtrl)
+
+	serviceMock.EXPECT().DeleteFunction(&lambda.DeleteFunctionInput{
+		FunctionName: aws.String("testfn"),
+	}).Return(&lambda.DeleteFunctionOutput{}, nil)
+
+	fn := &function.Function{
+		Config:  function.Config{Name: "testfn"},
+		Service: serviceMock,
+	}
+	err := fn.Delete()
+
+	assert.Nil(t, err)
+}
+
+func TestDelete_Failed(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	serviceMock := mock_lambdaiface.NewMockLambdaAPI(mockCtrl)
+
+	serviceMock.EXPECT().DeleteFunction(gomock.Any()).Return(nil, errors.New("API err"))
+
+	fn := &function.Function{
+		Config:  function.Config{Name: "testfn"},
+		Service: serviceMock,
+	}
+	err := fn.Delete()
+
+	assert.Error(t, err, "API err")
+}
