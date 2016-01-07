08d8523252f8d92f6fdef48834c568542ed754ff
Maciej Winnicki <maciej.winnicki@gmail.com>
2016-01-07 18:04:20 +0100

    Shell autocomplete

---
+++ contrib/completion/apex-completion.bash
@@ -0,0 +1,94 @@
+
+_apex()
+{
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( $( compgen -W '-h --help -h --help -V --version rollback invoke deploy list build delete' -- $cur) )
+    else
+        case ${COMP_WORDS[1]} in
+            rollback)
+            _apex_rollback
+        ;;
+            invoke)
+            _apex_invoke
+        ;;
+            deploy)
+            _apex_deploy
+        ;;
+            list)
+            _apex_list
+        ;;
+            build)
+            _apex_build
+        ;;
+            delete)
+            _apex_delete
+        ;;
+        esac
+
+    fi
+}
+
+_apex_rollback()
+{
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ $COMP_CWORD -ge 2 ]; then
+        COMPREPLY=( $( compgen -fW ' ' -- $cur) )
+    fi
+}
+
+_apex_invoke()
+{
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ $COMP_CWORD -ge 2 ]; then
+        COMPREPLY=( $( compgen -fW '-a --async -v --verbose ' -- $cur) )
+    fi
+}
+
+_apex_deploy()
+{
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ $COMP_CWORD -ge 2 ]; then
+        COMPREPLY=( $( compgen -fW ' ' -- $cur) )
+    fi
+}
+
+_apex_list()
+{
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ $COMP_CWORD -ge 2 ]; then
+        COMPREPLY=( $( compgen -W ' ' -- $cur) )
+    fi
+}
+
+_apex_build()
+{
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ $COMP_CWORD -ge 2 ]; then
+        COMPREPLY=( $( compgen -fW ' ' -- $cur) )
+    fi
+}
+
+_apex_delete()
+{
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ $COMP_CWORD -ge 2 ]; then
+        COMPREPLY=( $( compgen -fW ' ' -- $cur) )
+    fi
+}
+
+complete -F _apex apex
