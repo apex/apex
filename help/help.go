a008aef368db35a5db63e6560dd80d6b643490e7
TJ Holowaychuk <tj@vision-media.ca>
2016-01-17 15:41:14 -0800

    refactor help to support links
    now you can cmd+double click the links

--- help/help.go
+++ help/help.go
@@ -8,10 +8,11 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/mitchellh/go-wordwrap"
+	"golang.org/x/net/html"
 )
 
+// TODO: handle invalid page
 // TODO: HelpTopics could be interactive
-// TODO: walk nodes to highlight inline code etc
 // TODO: ~/.apex.json user config, use here for color mapping etc
 
 // colors.
@@ -38,7 +39,7 @@ func HelpTopics(w io.Writer) error {
 	defer fmt.Fprintf(w, "\n")
 
 	doc.Find(`#wiki-content .markdown-body ul li`).Each(func(i int, s *goquery.Selection) {
-		strs := strings.Split(text(s.Text()), ": ")
+		strs := strings.Split(text(s), ": ")
 		fmt.Fprintf(w, "  \033[%dm%s\033[0m: %s \n", blue, strs[0], strs[1])
 	})
 
@@ -54,32 +55,52 @@ func HelpTopic(topic string, w io.Writer) error {
 		return err
 	}
 
-	fmt.Fprintf(w, "\n")
-	defer fmt.Fprintf(w, "\n")
-
-	doc.Find(`#wiki-content .markdown-body *`).Each(func(i int, s *goquery.Selection) {
-		switch node := s.Get(0); node.Data {
-		case "h1":
-			fmt.Printf(" \033[%dm# %s\033[0m\n\n", blue, text(s.Text()))
-		case "h2":
-			fmt.Printf(" \033[%dm## %s\033[0m\n\n", blue, text(s.Text()))
-		case "h3":
-			fmt.Printf(" \033[%dm### %s\033[0m\n\n", blue, text(s.Text()))
-		case "p":
-			fmt.Printf("\033[%dm%s\033[0m\n\n", none, indent(text(s.Text()), 1))
-		case "div", "pre":
-			if s.HasClass("highlight") || node.Data == "pre" {
-				fmt.Printf("\033[%dm%s\033[0m\n\n", gray, indent(text(s.Text()), 2))
-			}
-		}
-	})
+	fmt.Fprintln(w)
+	fmt.Fprint(w, nodes(doc.Find(`#wiki-content .markdown-body`)))
+	fmt.Fprintln(w)
 
 	return nil
 }
 
-// text trim and wrap.
-func text(s string) string {
-	return wordwrap.WrapString(strings.TrimSpace(s), 80)
+// nodes returns a string representation of the selection's children.
+func nodes(s *goquery.Selection) string {
+	return strings.Join(s.Children().Map(node), "")
+}
+
+// contents returns a string representation of the selection's contents.
+func contents(s *goquery.Selection) string {
+	return strings.Join(s.Contents().Map(node), "")
+}
+
+// node returns a string representation of the selection.
+func node(i int, s *goquery.Selection) string {
+	switch node := s.Get(0); {
+	case node.Data == "h1":
+		return fmt.Sprintf(" \033[%dm# %s\033[0m\n\n", blue, text(s))
+	case node.Data == "h2":
+		return fmt.Sprintf(" \033[%dm## %s\033[0m\n\n", blue, text(s))
+	case node.Data == "h3":
+		return fmt.Sprintf(" \033[%dm### %s\033[0m\n\n", blue, text(s))
+	case node.Data == "p":
+		return fmt.Sprintf("\033[%dm%s\033[0m\n\n", none, indent(text(s), 1))
+	case node.Data == "pre" || s.HasClass("highlight"):
+		return fmt.Sprintf("\033[%dm%s\033[0m\n\n", gray, indent(text(s), 2))
+	case node.Data == "a":
+		return fmt.Sprintf("%s (%s) ", s.Text(), s.AttrOr("href", "missing link"))
+	case node.Data == "li":
+		return fmt.Sprintf("  • %s\n", contents(s))
+	case node.Data == "ul":
+		return fmt.Sprintf("%s\n", nodes(s))
+	case node.Type == html.TextNode:
+		return strings.TrimSpace(node.Data)
+	default:
+		return ""
+	}
+}
+
+// text of selection, trimmed and wrapped.
+func text(s *goquery.Selection) string {
+	return wordwrap.WrapString(strings.TrimSpace(s.Text()), 80)
 }
 
 // indent string N times.
