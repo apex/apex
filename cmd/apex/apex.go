2f9dd8d5380792998c5dee2189171dd06bbdcd39
Pilwon Huh <pilwon@gmail.com>
2016-01-18 20:23:28 -0500

    Minor refactoring. Swapped places

--- cmd/apex/apex.go
+++ cmd/apex/apex.go
@@ -12,13 +12,6 @@ var rootCmd = &cobra.Command{
 func init() {
 	pf := rootCmd.PersistentFlags()
 
-	pf.StringVarP(&pv.Chdir, "chdir", "C", "", "Working directory")
-	pf.BoolVarP(&pv.DryRun, "dry-run", "D", false, "Perform a dry-run")
-	pf.StringSliceVarP(&pv.Env, "env", "e", nil, "Environment variable")
-	pf.StringVarP(&pv.LogLevel, "log-level", "l", "info", "Log severity level")
-	pf.BoolVarP(&pv.Verbose, "verbose", "v", false, "Output verbose logs")
-	pf.BoolVarP(&pv.Yes, "yes", "y", false, "Automatic yes to prompts")
-
 	rootCmd.AddCommand(deployCmd)
 	rootCmd.AddCommand(deleteCmd)
 	rootCmd.AddCommand(invokeCmd)
@@ -28,6 +21,13 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 	rootCmd.AddCommand(wikiCmd)
 	rootCmd.AddCommand(versionCmd)
+
+	pf.StringVarP(&pv.Chdir, "chdir", "C", "", "Working directory")
+	pf.BoolVarP(&pv.DryRun, "dry-run", "D", false, "Perform a dry-run")
+	pf.StringSliceVarP(&pv.Env, "env", "e", nil, "Environment variable")
+	pf.StringVarP(&pv.LogLevel, "log-level", "l", "info", "Log severity level")
+	pf.BoolVarP(&pv.Verbose, "verbose", "v", false, "Output verbose logs")
+	pf.BoolVarP(&pv.Yes, "yes", "y", false, "Automatic yes to prompts")
 
 	// Add a newline at the top of each help message
 	rootCmd.SetHelpTemplate("\n" + rootCmd.HelpTemplate())
