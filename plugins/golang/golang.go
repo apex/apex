d61c4c9d5de4231521615cc708b70ff7b06a4ff6
TJ Holowaychuk <tj@vision-media.ca>
2016-01-26 13:29:20 -0800

    add support for overriding the build hook when using golang runtime
    this lets you use *.go etc if you have multiple main files while retaining these other hooks. we could do the
    same with the others later so its consistent but yea, need this right now ;D

--- plugins/golang/golang.go
+++ plugins/golang/golang.go
@@ -18,9 +18,12 @@ func (p *Plugin) Run(hook function.Hook, fn *function.Function) error {
 		return nil
 	}
 
+	if fn.Hooks.Build == "" {
+		fn.Hooks.Build = "GOOS=linux GOARCH=amd64 go build -o main main.go"
+	}
+
 	fn.Shim = true
 	fn.Runtime = "nodejs"
-	fn.Hooks.Build = "GOOS=linux GOARCH=amd64 go build -o main main.go"
 	fn.Hooks.Clean = "rm -f main"
 
 	return nil
