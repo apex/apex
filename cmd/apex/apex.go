ca3973b12cf531b9e91a7020493d3edc755b4817
TJ Holowaychuk <tj@vision-media.ca>
2016-01-28 11:44:24 -0800

    remove old --verbose flag

--- cmd/apex/apex.go
+++ cmd/apex/apex.go
@@ -29,7 +29,6 @@ func init() {
 	pf.BoolVarP(&pv.DryRun, "dry-run", "D", false, "Perform a dry-run")
 	pf.StringSliceVarP(&pv.Env, "env", "e", nil, "Environment variable")
 	pf.StringVarP(&pv.LogLevel, "log-level", "l", "info", "Log severity level")
-	pf.BoolVarP(&pv.Verbose, "verbose", "v", false, "Output verbose logs")
 	pf.BoolVarP(&pv.Yes, "yes", "y", false, "Automatic yes to prompts")
 
 	// Add a newline at the top of each help message
