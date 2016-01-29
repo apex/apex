2a171871f40f3fc1a237a93c3d1cbcbd3da79757
Maciej Winnicki <maciej.winnicki@gmail.com>
2016-01-29 11:20:52 +0100

    add tfvars output to list command. Closes #155

--- cmd/apex/apex_list.go
+++ cmd/apex/apex_list.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
+
+	"github.com/apex/log"
 
 	"github.com/spf13/cobra"
 )
 
+var listCmdLocalValues struct {
+	Tfvars bool
+}
+
 const listCmdExample = `  List all functions
-  $ apex list`
+  $ apex list
+
+  Output list as Terraform variables (.tfvars)
+  $ apex list --tfvars`
 
 var listCmd = &cobra.Command{
 	Use:     "list",
@@ -17,14 +25,41 @@ var listCmd = &cobra.Command{
 	Run:     listCmdRun,
 }
 
+func init() {
+	lv := &listCmdLocalValues
+	f := listCmd.Flags()
+
+	f.BoolVar(&lv.Tfvars, "tfvars", false, "Output as Terraform variables")
+}
+
 func listCmdRun(c *cobra.Command, args []string) {
-	fmt.Println()
+	lv := &listCmdLocalValues
 
 	err := pv.project.LoadFunctions()
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
 
+	if lv.Tfvars {
+		outputTfvars()
+	} else {
+		outputList()
+	}
+}
+
+func outputTfvars() {
+	for _, fn := range pv.project.Functions {
+		fnConfig, err := fn.GetConfig()
+		if err != nil {
+			log.Debugf("can't fetch function config: %s", err.Error())
+			continue
+		}
+		fmt.Printf("apex_function_%s=\"%s\"\n", fn.Name, *fnConfig.Configuration.FunctionArn)
+	}
+}
+
+func outputList() {
+	fmt.Println()
 	for _, fn := range pv.project.Functions {
 		fmt.Printf("  - %s (%s)\n", fn.Name, fn.Runtime)
 	}
