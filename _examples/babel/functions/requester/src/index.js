9fe33790d8696bb0c98429e1e9a96a1aea6c2224
TJ Holowaychuk <tj@vision-media.ca>
2016-01-30 20:21:37 -0800

    add babel example

---
+++ _examples/babel/functions/requester/src/index.js
@@ -0,0 +1,22 @@
+
+import axios from 'axios'
+import 'babel-polyfill'
+
+// Vanilla Lambda function.
+export default async function(e, ctx) {
+  console.log('fetching %d urls', e.urls.length)
+
+  try {
+    const res = await Promise.all(e.urls.map(async function(url){
+      console.log('fetching %s', url)
+      return {
+        status: (await axios.get(url)).status,
+        url
+      }
+    }))
+
+    ctx.succeed(res)
+  } catch (err) {
+    ctx.fail(err)
+  }
+}
