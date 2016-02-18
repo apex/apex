da2447331042d13600539127754b2c64504e3b3c
TJ Holowaychuk <tj@vision-media.ca>
2016-02-18 10:05:29 -0800

    fix webpack example

--- _examples/babel/webpack.config.js
+++ _examples/babel/webpack.config.js
@@ -1,20 +1,24 @@
 // NOTE: paths are relative to each functions folder
 
 module.exports = {
-    entry: './src/index.js',
-    target: 'node',
-    output: {
-        path: './lib',
-        filename: 'index.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: [/node_modules/]
-            }
-        ]
-    }
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: './lib',
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
 }
