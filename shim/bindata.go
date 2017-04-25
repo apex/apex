054c3a1a28c22f009b9eeec2cec078c9fcbbdbf7
TJ Holowaychuk <tj@vision-media.ca>
2017-04-25 14:56:10 -0700

    regenerate mock

--- shim/bindata.go
+++ shim/bindata.go
@@ -86,7 +86,7 @@ func bindataGo() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "bindata.go", size: 0, mode: os.FileMode(420), modTime: time.Unix(1491004871, 0)}
+	info := bindataFileInfo{name: "bindata.go", size: 0, mode: os.FileMode(420), modTime: time.Unix(1493157360, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
