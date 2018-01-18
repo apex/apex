cf2ec2bbc2862da30f75473ee881d312d854de39
TJ Holowaychuk <tj@vision-media.ca>
2018-01-17 16:26:42 -0800

    add new upgrade mechanism to match Up

--- upgrade/upgrade.go
+++ upgrade/upgrade.go
@@ -2,90 +2,65 @@
 package upgrade
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"runtime"
 
-	"github.com/apex/log"
-	"github.com/google/go-github/github"
+	"github.com/pkg/errors"
+	"github.com/tj/go-update"
+	"github.com/tj/go-update/stores/github"
+	"github.com/tj/go/term"
+
+	"github.com/apex/apex/internal/progressreader"
+	"github.com/apex/apex/internal/util"
 )
 
 // Upgrade the current `version` of apex to the latest.
 func Upgrade(version string) error {
-	log.Infof("current release is v%s", version)
+	term.HideCursor()
+	defer term.ShowCursor()
 
-	// fetch releases
-	gh := github.NewClient(nil)
-	releases, _, err := gh.Repositories.ListReleases(context.Background(), "apex", "apex", nil)
-	if err != nil {
-		return err
+	p := &update.Manager{
+		Command: "apex",
+		Store: &github.Store{
+			Owner:   "apex",
+			Repo:    "apex",
+			Version: version,
+		},
 	}
 
-	// see if it's new
-	latest := releases[0]
-	log.Infof("latest release is %s", *latest.TagName)
+	// fetch releases
+	releases, err := p.LatestReleases()
+	if err != nil {
+		return errors.Wrap(err, "fetching latest release")
+	}
 
-	if (*latest.TagName)[1:] == version {
-		log.Infof("you're up to date :)")
+	// no updates
+	if len(releases) == 0 {
+		util.LogPad("No updates available, you're good :)")
 		return nil
 	}
 
-	asset := findAsset(latest)
-	if asset == nil {
-		return errors.New("cannot find binary for your system")
+	// latest
+	r := releases[0]
+
+	// find the tarball for this system
+	a := r.FindTarball(runtime.GOOS, runtime.GOARCH)
+	if a == nil {
+		return errors.Errorf("failed to find a binary for %s %s", runtime.GOOS, runtime.GOARCH)
 	}
 
-	// get the executable's path
-	cmdPath, err := exec.LookPath("apex")
+	// download tarball to a tmp dir
+	tarball, err := a.DownloadProxy(progressreader.New)
 	if err != nil {
-		return err
-	}
-	cmdDir := filepath.Dir(cmdPath)
-
-	// create tmp file
-	tmpPath := filepath.Join(cmdDir, "apex-upgrade")
-	f, err := os.OpenFile(tmpPath, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0755)
-	if err != nil {
-		return err
+		return errors.Wrap(err, "downloading tarball")
 	}
 
-	// download binary
-	log.Infof("downloading %s", *asset.BrowserDownloadURL)
-	res, err := http.Get(*asset.BrowserDownloadURL)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	// copy it down
-	_, err = io.Copy(f, res.Body)
-	if err != nil {
-		return err
+	// install it
+	if err := p.Install(tarball); err != nil {
+		return errors.Wrap(err, "installing")
 	}
 
-	// replace it
-	log.Infof("replacing %s", cmdPath)
-	err = os.Rename(tmpPath, cmdPath)
-	if err != nil {
-		return err
-	}
+	term.ClearAll()
+	util.LogPad("Updated %s to %s", version, r.Version)
 
-	log.Infof("visit https://github.com/apex/apex/releases for the changelog")
-	return nil
-}
-
-// findAsset returns the binary for this platform.
-func findAsset(release *github.RepositoryRelease) *github.ReleaseAsset {
-	for _, asset := range release.Assets {
-		if *asset.Name == fmt.Sprintf("apex_%s_%s", runtime.GOOS, runtime.GOARCH) {
-			return &asset
-		}
-	}
 	return nil
 }
